11f18146104d4dfe83cab2ea4342fe81eb922562
Nancy <nancycode1995@proton.me>
2022-08-13 15:13:55 -0500

    Rewrite for performance

--- 2015/10.py
+++ 2015/10.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python3
 
+from itertools import groupby
+
 from advent import solution
 
-def consume_run(rest, run=""):
-    while run == "" or rest.startswith(run[0]):
-        run += rest[0]
-        rest = rest[1:]
-    return run, rest
-
-def look_and_say(rest, consumed=""):
-    while rest:
-        run, rest = consume_run(rest)
-        consumed += f"{len(run)}{run[0]}"
-    return consumed
-
-def do_iterations(string, iterations: int):
+def look_and_say(string, iterations=1):
     for i in range(iterations):
-        print(f"Iteration {i + 1}/{iterations}")
-        string = look_and_say(string)
-    return len(string)
+        print(f"Iteration {i}/{iterations} = {len(string)}")
+        string = "".join([f"{len(list(group))}{key}" for key, group in groupby(string)])
+    return string
 
 @solution("10.txt", "Part 1")
 def solve(string):
-    return do_iterations(string, 40)
+    return len(look_and_say(string, 40))
 
 @solution("10.txt", "Part 2")
 def solve(string):
-    return do_iterations(string, 50)
+    return len(look_and_say(string, 50))
