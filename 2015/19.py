b26c52cf7fd641b09cb3b53f833a5e187354be2f
Nancy <nancycode1995@proton.me>
2022-08-14 11:23:48 -0500

    Day 19 part 2 brute force

--- 2015/19.py
+++ 2015/19.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from functools import cache
+
 from advent import solution
 
 def patch(string, substring, index, length):
@@ -9,10 +11,9 @@ def patch(string, substring, index, length):
 def indices(string, substring):
     """Yield the indices of all occurences of the substring within the string."""
 
-    position = 0
-    while (index := string[position:].find(substring)) != -1:
-        yield position + index
-        position += index + 1
+    for i in range(len(string)):
+        if string[i:].startswith(substring):
+            yield i
 
 @solution("19.txt", "Part 1")
 def solve(string):
@@ -27,19 +28,31 @@ def solve(string):
 
 @solution("19.txt", "Part 2")
 def solve(string):
-    replacements_string, target = string.split("\n\n")
+    replacements_string, molecule = string.split("\n\n")
     replacements = [tuple(s.strip() for s in string.split("=>")) for string in replacements_string.split("\n")]
 
-    def fewest_steps(molecule):
-        if molecule == target:
-            return 0
-        if len(molecule) > len(target):
-            return None
-        def tree():
-            for source, replacement in replacements:
-                for index in indices(molecule, source):
-                    replaced = patch(molecule, replacement, index, len(source))
-                    yield fewest_steps(replaced)
-        return min(filter(lambda x: x != None, tree()))
+    def replace(molecule):
+        """Yield all possible BACKWARDS replacements given a molecule."""
+        for source, replacement in replacements:
+            for index in indices(molecule, replacement):
+                yield patch(molecule, source, index, len(replacement))
 
-    return fewest_steps("e")
+    @cache
+    def search(molecule, target="e", depth=0):
+        """Perform backwards depth-first brute-force search."""
+        # If we found the target, cease recursion and return depth
+        if molecule == target:
+            return depth
+        # Get the next level of the tree
+        # Filter out dead ends
+        generation = [search(replaced, target, depth + 1) for replaced in replace(molecule)]
+        filtered = list(filter(lambda x: x != -1, generation))
+        # As we are searching for the shortest path,
+        # take the minimum steps
+        if filtered:
+            return filtered[0] if len(filtered) == 1 else min(*filtered)
+        # If we arrive at a dead end (no results), cease recursion and return -1 to signify target not found
+        else:
+            return -1
+
+    return search(molecule)
