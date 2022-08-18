097463faf7381db736b34b72a9c2333a45a29a87
Nancy <nancycode1995@proton.me>
2022-08-18 18:24:52 -0500

    Day 1

--- 2015/solutions/c/src/days/1.c
+++ 2015/solutions/c/src/days/1.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+
 #include "solution.h"
 
 void solution_day_1_part_1(char *input, char *output) {
-
+    int sum = 0;
+    char character;
+    while ((character = *(input++)))
+        sum += character == '(' ? 1 : -1;
+    sprintf(output, "%d", sum);
 }
 
 void solution_day_1_part_2(char *input, char *output) {
-
+    int sum = 0;
+    int i;
+    char character;
+    for (i = 0; (character = *(input++)) && sum >= 0; i++)
+        sum += character == '(' ? 1 : -1;
+    sprintf(output, "%d", i);
 }
